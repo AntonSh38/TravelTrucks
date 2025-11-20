94c390224df4a4bbd89dd9fde34ca989cdd511ba
Anton Shuvalov <anshuvalov83@gmail.com>
2025-11-20 12:54:59 +0200

    add Catalog id page

--- app/catalog/[id]/page.tsx
+++ app/catalog/[id]/page.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import css from './Page.module.css';
-
 export default function CamperDetailPage() {
-  return <div className={css['page']}>Test Page</div>;
+  return <div>Test Page</div>;
 }
